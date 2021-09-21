3b7c629392f12b90dea1d1269e1e77356373efb8
Barker French <barkerfrench1@gmail.com>
2021-09-20 20:49:40 -0400

    resolved conflict

--- minus.py
+++ minus.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
+# this function makes a lot of sense
 def minus (a, b, c):
     return (a - b -c)
-=======
-# this function makes a lot of sense
-def minus (a, b):
-    return (a - b)
->>>>>>> 0d8f02ff322230614ea3ceb5a06363a22d459fd3
+
